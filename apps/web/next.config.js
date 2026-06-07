4ed87d8f04d76b622c5c7b2a072dcb90b4baa7ba
Raju H R <learnandgrow585@gmail.com>
2026-06-07 18:17:40 +0530

    Add Vercel monorepo build config + production next.config tweaks

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   transpilePackages: ['@planforge/shared-types'],
+  // Don't fail the production build on lint warnings (types are still checked)
+  eslint: { ignoreDuringBuilds: true },
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
 };
 
 module.exports = nextConfig;
